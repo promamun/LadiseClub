7eb46707373c0c1e70ea4295c07d15bcd1c2b787
arifbillahshobuz507 <arifbillahmahamudaamena@gmail.com>
2024-06-03 22:43:33 +0600

    notice and event modify backend

--- resources/views/content/notice/noticeEdit.blade.php
+++ resources/views/content/notice/noticeEdit.blade.php
@@ -26,63 +26,66 @@
                 <h4 class="mb-1 mt-3">Update Notice</h4>
             </div>
             <div class="d-flex align-content-center flex-wrap gap-3">
-                <a href="{{ route('notice-list') }}"><button type="submit" class="btn btn-primary">Notice List</button></a>
+              <a href="{{ route('notice-list') }}"><button type="submit" class="btn btn-primary">Notice List</button></a>
             </div>
         </div>
+
         <div class="row">
-          <!-- First column-->
-           <!-- First column-->
-           <div class="col-12 col-lg-12">
-            <div class="col-sm-12">
-              <div class="row">
-                  <div class="col-sm-12">
-                      @if (session('error'))
-                          <div class="alert alert-danger">
-                              {{ session('error') }}
-                          </div>
-                      @endif
-                  </div>
-              </div>
-          </div>
-          <div class="col-12 col-lg-12">
-              <!-- Product Information -->
-              <div class="card mb-4">
+            <!-- First column-->
+            <div class="col-12 col-lg-12">
+              <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-12">
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+                <!-- Product Information -->
+                <div class="card mb-4">
                   <form action="{{ route('notice.update', $data->id) }}" method="POST" enctype="multipart/form-data">
-                      <div class="card-header">
-                          <h5 class="card-title mb-0">Notice</h5>
-                      </div>
-                      @csrf
-                      <div class="card-body">
-                          <div class="row mb-3">
-                              <div class="col">
-                                  <label class="form-label" for="name">Name</label><span class="text-info">(optional)</span>
-                                  <input type="text" class="form-control" id="name" placeholder="Name Here" value="{{ old('name')?? $data->name }}" name="name" aria-label="Name Here">
-                              </div>
-                              <div class="col">
-                                <label class="form-label" for="image">Image</label><span class="text-info">(optional)</span>
-                                <input type="file" class="form-control" id="image" placeholder="Image Here" name="image" aria-label="Image">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Notice</h5>
+                    </div>
+                    @csrf
+                    <div class="card-body">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label class="form-label" for="name">Name</label><span class="text-info">(optional)</span>
+                                <input type="text" class="form-control" id="name" placeholder="Name Here" value="{{ old('name')?? $data->name }}" name="name" aria-label="Name Here">
                             </div>
+                            <div class="col">
+                              <label class="form-label" for="image">Image</label><span class="text-info">(optional)</span>
+                              <input type="file" class="form-control" id="image" placeholder="Image Here" name="image" aria-label="Image">
                           </div>
-                          <div class="row mb-3">
-                            <div class="col">
-                                <label class="form-label" for="description">Description</label><span class="text-info">(optional)</span>
-                                <input type="text" class="form-control" id="description" placeholder="Description Here" value="{{ old('description')?? $data->description }}" name="description" aria-label="description Here">
-                            </div>
-                            <div class="col">
-                                <label class="form-label" for="short_description">Short Description</label><span class="text-info">(optional)</span>
-                                <input type="text" class="form-control" id="short_description" placeholder="Short Description Here" value="{{ old('short_description')?? $data->short_description }}" name="short_description" aria-label="short_description Here">
-                            </div>
                         </div>
-                          <div class="pt-4 mb-3 float-lg-end">
-                              <button type="submit" class="btn btn-primary me-sm-3 me-1 waves-effect waves-light">Update</button>
+                        <div class="row mb-3">
+                          <div class="col">
+                              <label class="form-label" for="description">Description</label><span class="text-info">(optional)</span>
+                              <input type="text" class="form-control" id="description" placeholder="Description Here" value="{{ old('description')?? $data->description }}" name="description" aria-label="description Here">
+                          </div>
+                          <div class="col">
+                              <label class="form-label" for="short_description">Short Description</label><span class="text-info">(optional)</span>
+                              <input type="text" class="form-control" id="short_description" placeholder="Short Description Here" value="{{ old('short_description')?? $data->short_description }}" name="short_description" aria-label="short_description Here">
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <div class="col">
+                            <img width="100" src="{{ asset('notice/' . $data->image) }}" alt="img">
                           </div>
                       </div>
-                  </form>
-              </div>
-              <!-- /Product Information -->
-          </div>
-          <!-- /Second column -->
+                        <div class="pt-4 mb-3 float-lg-end">
+                            <button type="submit" class="btn btn-primary me-sm-3 me-1 waves-effect waves-light">Update</button>
+                        </div>
+                    </div>
+                </form>
+                </div>
+                <!-- /Product Information -->
+            </div>
+            <!-- /Second column -->
         </div>
     </div>
 @endsection
-
